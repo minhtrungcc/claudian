66d05d15757f6e297dfaddde365adbfffdf3c619
minhtrungcc <minhtrung@pm.me>
2026-04-17 11:04:10 +0700

    feat(acp): add GEMINI_ environment key patterns

--- src/providers/acp/registration.ts
+++ src/providers/acp/registration.ts
@@ -1,21 +1,21 @@
 import type { ProviderRegistration } from '../../core/providers/types';
-import { AcpChatRuntime } from './runtime/AcpChatRuntime';
-import { AcpTitleGenerationService } from './auxiliary/AcpTitleGenerationService';
-import { AcpInstructionRefineService } from './auxiliary/AcpInstructionRefineService';
 import { AcpInlineEditService } from './auxiliary/AcpInlineEditService';
-import { AcpConversationHistoryService } from './history/AcpConversationHistoryService';
+import { AcpInstructionRefineService } from './auxiliary/AcpInstructionRefineService';
 import { AcpTaskResultInterpreter } from './auxiliary/AcpTaskResultInterpreter';
+import { AcpTitleGenerationService } from './auxiliary/AcpTitleGenerationService';
 import { ACP_PROVIDER_CAPABILITIES } from './capabilities';
 import { acpSettingsReconciler } from './env/AcpSettingsReconciler';
-import { acpChatUIConfig } from './ui/AcpChatUIConfig';
+import { AcpConversationHistoryService } from './history/AcpConversationHistoryService';
+import { AcpChatRuntime } from './runtime/AcpChatRuntime';
 import { getAcpProviderSettings } from './settings';
+import { acpChatUIConfig } from './ui/AcpChatUIConfig';
 
 export const acpProviderRegistration: ProviderRegistration = {
   displayName: 'ACP',
   blankTabOrder: 30,
   isEnabled: (settings) => getAcpProviderSettings(settings).enabled,
   capabilities: ACP_PROVIDER_CAPABILITIES,
-  environmentKeyPatterns: [/^ACP_/i],
+  environmentKeyPatterns: [/^ACP_/i, /^GEMINI_/i],
   chatUIConfig: acpChatUIConfig,
   settingsReconciler: acpSettingsReconciler,
   createRuntime: ({ plugin }) => new AcpChatRuntime(plugin),
